f57fd10e1ac0d5748e0fe00b951323263f5e4d54
Henri Drake <henrifdrake@vcv069042.vpn.uci.edu>
2023-08-22 10:25:08 -0700

    Renamed and removed unnecessary code
    - Renamed from `diffusive_convection.jl` to `horizontal_diffusion.jl` since there is no "convection"
    in the purely diffusive problem
    
    - Removed all mentions of "flow" and velocity/vorticity diagnostics because they are meaningless in
    the diffusive problem.
    
    - Updates the comments in the script to reflect the purpose of this script

--- horizontal_diffusion.jl
+++ horizontal_diffusion.jl
@@ -1,11 +1,12 @@
-# # Horizontal convection example
+# # Horizontal diffusion example
 #
 # In "horizontal convection", a non-uniform buoyancy is imposed on top of an initially resting fluid.
+# By "horizontal diffusion", we refer to the hypothetical solution of the identically-configured problem 
+# but in which there is no buoyancy advection (or turbulence). The difference between the two solutions is
+# a useful way of quantifying the degree of turbulence in horizontal convection problems.
 #
-# This example demonstrates:
-#
-#   * How to use computed `Field`s for output.
-#   * How to post-process saved output using `FieldTimeSeries`.
+# This script serves to verify the Nusselt number calculation in the Oceananigans.jl 
+# "horizontal_convection" example in the documentation.
 #
 # ## Install dependencies
 #
@@ -16,11 +17,11 @@
 # pkg"add Oceananigans, CairoMakie"
 # ```
 
-# ## Horizontal convection
+# ## Horizontal diffusion
 #
-# We consider two-dimensional horizontal convection of an incompressible flow ``\boldsymbol{u} = (u, w)``
-# on the ``(x, z)``-plane (``-L_x/2 \le x \le L_x/2`` and ``-H \le z \le 0``). The flow evolves
-# under the effect of gravity. The only forcing on the fluid comes from a prescribed, non-uniform
+# We consider two-dimensional horizontal diffusion problem 
+# on the ``(x, z)``-plane (``-L_x/2 \le x \le L_x/2`` and ``-H \le z \le 0``).
+# The only forcing on the fluid comes from a prescribed, non-uniform
 # buoyancy at the top-surface of the domain.
 #
 # We start by importing `Oceananigans` and `Printf`.
@@ -54,32 +55,25 @@ b_bcs = FieldBoundaryConditions(top = ValueBoundaryCondition(bˢ, parameters=(; 
 
 # ### Non-dimensional control parameters and Turbulence closure
 #
-# The problem is characterized by three non-dimensional parameters. The first is the domain's
-# aspect ratio, ``L_x / H`` and the other two are the Rayleigh (``Ra``) and Prandtl (``Pr``)
-# numbers:
+# The problem is characterized by two non-dimensional parameters. The first is the domain's
+# aspect ratio, ``L_x / H`` and the other is the Rayleigh (``Ra``) number:
 #
 # ```math
-# Ra = \frac{b_* L_x^3}{\nu \kappa} \, , \quad \text{and}\, \quad Pr = \frac{\nu}{\kappa} \, .
+# Ra = \frac{b_* L_x^3}{\kappa^2}.
 # ```
 #
-# The Prandtl number expresses the ratio of momentum over heat diffusion; the Rayleigh number
-# is a measure of the relative importance of gravity over viscosity in the momentum equation.
-#
-# For a domain with a given extent, the nondimensional values of ``Ra`` and ``Pr`` uniquely
-# determine the viscosity and diffusivity, i.e.,
+# For a domain with a given extent, the nondimensional value of ``Ra`` determines the diffusivity, i.e.,
 # 
 # ```math
-# \nu = \sqrt{\frac{Pr b_* L_x^3}{Ra}} \quad \text{and} \quad \kappa = \sqrt{\frac{b_* L_x^3}{Pr Ra}} \, .
+# \kappa = \sqrt{\frac{b_* L_x^3}{Ra}} \, .
 # ```
 #
-# We use isotropic viscosity and diffusivities, `ν` and `κ` whose values are obtain from the
-# prescribed ``Ra`` and ``Pr`` numbers. Here, we use ``Pr = 1`` and ``Ra = 10^8``:
+# We use an isotropic diffusivity, `κ` whose value is obtained from the
+# prescribed ``Ra`` numbers. Here, we use ``Ra = 10^8``:
 
-Pr = 1.0    # Prandtl number
 Ra = 1e8    # Rayleigh number
 
-ν = sqrt(Pr * b★ * Lx^3 / Ra)  # Laplacian viscosity
-κ = ν * Pr                     # Laplacian diffusivity
+κ = sqrt(b★ * Lx^3 / Ra)                  # Laplacian diffusivity
 nothing # hide
 
 # ## Model instantiation
@@ -92,23 +86,22 @@ model = NonhydrostaticModel(; grid,
                             timestepper = :RungeKutta3,
                             tracers = :b,
                             buoyancy = BuoyancyTracer(),
-                            closure = ScalarDiffusivity(; ν, κ),
+                            closure = ScalarDiffusivity(; κ),
                             boundary_conditions = (; b=b_bcs))
 
 # ## Simulation set-up
 #
-# We set up a simulation that runs up to ``t = 40`` with a `JLD2OutputWriter` that saves the flow
-# speed, ``\sqrt{u^2 + w^2}``, the buoyancy, ``b``, and the vorticity, ``\partial_z u - \partial_x w``.
+# We set up a simulation that runs up to ``t = 1000`` with a `JLD2OutputWriter` that saves the buoyancy, ``b``.
 
-simulation = Simulation(model, Δt=0.1, stop_time=100.0)
+simulation = Simulation(model, Δt=0.1, stop_time=1000.0)
 
 # ### The `TimeStepWizard`
 #
-# The `TimeStepWizard` manages the time-step adaptively, keeping the Courant-Freidrichs-Lewy 
-# (CFL) number close to `0.75` while ensuring the time-step does not increase beyond the 
-# maximum allowable value for numerical stability.
+# The `TimeStepWizard` manages the time-step adaptively. Since there is no advection in this problem,
+# we set the Courant-Freidrichs-Lewy (CFL) number to Inf since it is irrelevant. A maximum non-dimentional timestep of
+# well below 1 ensures that the diffusive CFL condition is satisfied.
 
-wizard = TimeStepWizard(cfl=1e3, max_change=1.25, max_Δt=0.1)
+wizard = TimeStepWizard(cfl=Inf, max_change=1.25, max_Δt=0.1)
 
 simulation.callbacks[:wizard] = Callback(wizard, IterationInterval(100))
 
@@ -116,9 +109,9 @@ simulation.callbacks[:wizard] = Callback(wizard, IterationInterval(100))
 #
 # We write a function that prints out a helpful progress message while the simulation runs.
 
-progress(sim) = @printf("i: % 6d, sim time: % 1.3f, wall time: % 10s, Δt: % 1.4f, advective CFL: %.2e, diffusive CFL: %.2e\n",
+progress(sim) = @printf("i: % 6d, sim time: % 1.3f, wall time: % 10s, Δt: % 1.4f, diffusive CFL: %.2e\n",
                         iteration(sim), time(sim), prettytime(sim.run_wall_time),
-                        sim.Δt, AdvectiveCFL(sim.Δt)(sim.model), DiffusiveCFL(sim.Δt)(sim.model))
+                        sim.Δt, DiffusiveCFL(sim.Δt)(sim.model))
 
 simulation.callbacks[:progress] = Callback(progress, IterationInterval(100))
 
@@ -128,14 +121,8 @@ simulation.callbacks[:progress] = Callback(progress, IterationInterval(100))
 # and the buoyancy, ``b``. Note that computed `Field`s take "AbstractOperations" on `Field`s as
 # input:
 
-u, v, w = model.velocities # unpack velocity `Field`s
 b = model.tracers.b        # unpack buoyancy `Field`
 
-## total flow speed
-s = @at (Center, Center, Center) sqrt(u^2 + w^2)
-
-## y-component of vorticity
-ζ = ∂z(u) - ∂x(w)
 nothing # hide
 
 # We create a `JLD2OutputWriter` that saves the speed, and the vorticity. Because we want
@@ -146,9 +133,9 @@ nothing # hide
 #
 # We then add the `JLD2OutputWriter` to the `simulation`.
 
-saved_output_filename = "diffusive_convection.jld2"
+saved_output_filename = "horizontal_diffusion.jld2"
 
-simulation.output_writers[:fields] = JLD2OutputWriter(model, (; s, b, ζ),
+simulation.output_writers[:fields] = JLD2OutputWriter(model, (; b),
                                                       schedule = TimeInterval(5),
                                                       filename = saved_output_filename,
                                                       with_halos = true,
@@ -173,18 +160,15 @@ using Oceananigans
 using Oceananigans.Fields
 using Oceananigans.AbstractOperations: volume
 
-saved_output_filename = "diffusive_convection.jld2"
+saved_output_filename = "horizontal_diffusion.jld2"
 
 ## Open the file with our data
-s_timeseries = FieldTimeSeries(saved_output_filename, "s")
 b_timeseries = FieldTimeSeries(saved_output_filename, "b")
-ζ_timeseries = FieldTimeSeries(saved_output_filename, "ζ")
 
 times = b_timeseries.times
 
 ## Coordinate arrays
 xc, yc, zc = nodes(b_timeseries[1])
-xζ, yζ, zζ = nodes(ζ_timeseries[1])
 nothing # hide
 
 χ_timeseries = deepcopy(b_timeseries)
@@ -194,7 +178,6 @@ for i in 1:length(times)
   χ_timeseries[i] .= @at (Center, Center, Center) κ * (∂x(bᵢ)^2 + ∂z(bᵢ)^2)
 end
 
-
 # Now we're ready to animate using Makie.
 
 @info "Making an animation from saved data..."
@@ -203,14 +186,10 @@ n = Observable(1)
 
 title = @lift @sprintf("t=%1.2f", times[$n])
 
-sₙ = @lift interior(s_timeseries[$n], :, 1, :)
-ζₙ = @lift interior(ζ_timeseries[$n], :, 1, :)
 bₙ = @lift interior(b_timeseries[$n], :, 1, :)
 χₙ = @lift interior(χ_timeseries[$n], :, 1, :)
 
-slim = 0.6
 blim = 1
-ζlim = 9
 χlim = 0.025
 
 axis_kwargs = (xlabel = L"x / H",
@@ -219,36 +198,20 @@ axis_kwargs = (xlabel = L"x / H",
                aspect = Lx / H,
                titlesize = 20)
 
-fig = Figure(resolution = (600, 1100))
+fig = Figure(resolution = (600, 600))
 
-ax_s = Axis(fig[2, 1];
-            title = L"speed, $(u^2+w^2)^{1/2} / (L_x b_*) ^{1/2}", axis_kwargs...)
-
-ax_b = Axis(fig[3, 1];
+ax_b = Axis(fig[2, 1];
             title = L"buoyancy, $b / b_*$", axis_kwargs...)
 
-ax_ζ = Axis(fig[4, 1];
-            title = L"vorticity, $(∂u/∂z - ∂w/∂x) \, (L_x / b_*)^{1/2}$", axis_kwargs...)
-
-ax_χ = Axis(fig[5, 1];
+ax_χ = Axis(fig[3, 1];
             title = L"buoyancy dissipation, $κ |\mathbf{\nabla}b|^2 \, (L_x / {b_*}^5)^{1/2}$", axis_kwargs...)
 
 fig[1, :] = Label(fig, title, fontsize=24, tellwidth=false)
-
-hm_s = heatmap!(ax_s, xc, zc, sₙ;
-                colorrange = (0, slim),
-                colormap = :speed)
-Colorbar(fig[2, 2], hm_s)
 
 hm_b = heatmap!(ax_b, xc, zc, bₙ;
                 colorrange = (-blim, blim),
                 colormap = :thermal)
 Colorbar(fig[3, 2], hm_b)
-
-hm_ζ = heatmap!(ax_ζ, xζ, zζ, ζₙ;
-                colorrange = (-ζlim, ζlim),
-                colormap = :balance)
-Colorbar(fig[4, 2], hm_ζ)
 
 hm_χ = heatmap!(ax_χ, xc, zc, χₙ;
                 colorrange = (0, χlim),
@@ -259,26 +222,24 @@ Colorbar(fig[5, 2], hm_χ)
 
 frames = 1:length(times)
 
-record(fig, "diffusive_convection.mp4", frames, framerate=8) do i
+record(fig, "horizontal_diffusion.mp4", frames, framerate=8) do i
     msg = string("Plotting frame ", i, " of ", frames[end])
     print(msg * " \r")
     n[] = i
 end
 nothing #hide
 
-# ![](horizontal_convection.mp4)
-
-
-# At higher Rayleigh numbers the flow becomes much more vigorous. See, for example, an animation
-# of the voricity of the fluid at ``Ra = 10^{12}`` on [vimeo](https://vimeo.com/573730711). 
+# ![](horizontal_diffusion.mp4)
 
 # ### Validation of the diffusive solution in Oceananigans.jl
 
 χ_diff_analytical = κ * b★^2 * π * tanh(2π * H / Lx) / (Lx * H)
 nothing # hide
 
-# We recover the time from the saved `FieldTimeSeries` and construct two empty arrays to store
-# the volume-averaged kinetic energy and the instantaneous Nusselt number,
+# We recover the time from the saved `FieldTimeSeries` and construct an empty arrays to store
+# the volume-averaged equilibration ratio (the ratio of the instantaneous volume-integrated buoyancy
+# dissipation in a diffusive Oceananigans.jl simulation versus the equilibrium analytical solution.
+# The equilibration ratio should asymptote to 1 as the simulation reaches its equilibrium state.
 
 t = b_timeseries.times
 
