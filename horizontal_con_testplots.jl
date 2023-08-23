f472f90d29c296356dd4c3e9c1ae075bb1b92507
ikeshwani <ikeshwan@uci.edu>
2023-08-22 18:05:49 -0700

    Lines 56-end: Attempted to plot chi (buoyancy diffusion) for both advection schemes (turbulent and diffusive) over time.

--- horizontal_con_testplots.jl
+++ horizontal_con_testplots.jl
@@ -2,6 +2,7 @@ using CairoMakie
 using Oceananigans
 using Oceananigans.Fields
 using Oceananigans.AbstractOperations: volume
+using Statistics
 
 saved_output_filename = "horizontal_convection.jld2"
 
@@ -51,3 +52,45 @@ b_nans_meanx = nanmean(b_nans, dims=(1,2))[1,1,:,:]
 b_nans_meanx_perm = permutedims(b_nans_meanx, (2,1))
 
 heatmap(times, zc, b_nans_meanx_perm)
+
+#Now that we have run an experiment with the two advection schemes 
+# turbulent and diffusive we can analyze those to plot the Nusselt Number
+
+#first open the file with our χ data
+
+b_timeseries_diffusive = FieldTimeSeries("diffusive_convection_hills.jld2", "b")
+times_diff = b_timeseries_diffusive.times
+χ_timeseries_diffusive = FieldTimeSeries("diffusive_convection_hills.jld2", "χ")
+
+b_timeseries_turbulent = FieldTimeSeries("turbulent_convection_hills.jld2", "b")
+times_turb = b_timeseries_turbulent.times
+χ_timeseries_turbulent = FieldTimeSeries("turbulent_convection_hills.jld2", "χ")
+
+#χₙ_diffusive = @lift interior(χ_timeseries_diffusive[$n], :, 1, :)
+#χₙ_turbulent = @lift interior(χ_timeseries_turbulent[$n], :, 1, :)
+
+
+for i = 1:length(times_diff)
+    χ_diff_diffusive = Field(Integral(χ_timeseries_diffusive[i]))
+    compute!(χ_diff_diffusive)
+
+    χ_diff_turbulent = Field(Integral(χ_timeseries_turbulent[i]))
+    compute!(χ_diff_turbulent)
+    
+end
+
+
+#lets plot the buoyancy dissipation over time for each scheme
+
+
+fig = Figure(resolution = (600,400))
+ax_diff = Axis(fig[1,1], xlabel= L"t \, (b_* / L_x) ^{1/2}", ylabel = "χ Diffusive", title="Diffusive Buoyancy Dissipation vs Time")
+lines!(ax_diff, times_diff, χ_diff_diffusive)
+
+ax_turb = Axis(fig[2, 1], xlabel= L"t \, (b_* / L_x) ^{1/2}", ylabel = "χ Turbulent", title = "Turbulent Buoyancy Dissipation vs Time")
+lines!(ax_turb, times_turb, χ_diff_turbulent)
+
+current_figure()
+fig
+
+
