c8309d1aefbe9bb4b3452ad57e10d98b0a53f735
Isba Keshwani <ikeshwan@uci.edu>
2024-09-04 14:00:59 -0700

    added HPC3 changes to notebook

--- scripts/run_thc.jl
+++ scripts/run_thc.jl
@@ -4,7 +4,7 @@ using TopographicHorizontalConvection: HorizontalConvectionSimulation
 #simulation = HorizontalConvectionSimulation(Ra=1e7, h₀_frac=0.6, Nx=256, Ny=1, Nz=32, advection=true)
 #simulation = HorizontalConvectionSimulation(Ra=1e7, h₀_frac=0.6, Nx=256, Ny=1, Nz=32, advection=false)
 #simulation = HorizontalConvectionSimulation(Ra=1e8, h₀_frac=0.6, Nx=256, Ny=1, Nz=32, advection=true)
-simulation = HorizontalConvectionSimulation(Ra=1e8, h₀_frac=0.6, Nx=256, Ny=1, Nz=32, b_init=-0.5, advection=true)
+simulation = HorizontalConvectionSimulation(Ra=1e8, h₀_frac=0.6, Nx=951, Ny=1, Nz=119, b_init=-0.5, advection=true) #new res to resolve kolmogorov scale
 
 run!(simulation, pickup=false)
 
